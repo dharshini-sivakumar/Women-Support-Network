2575aea9ea28fefbc9c0577bc13047d1080aa152
Hacker <hacker@demo.com>
2026-03-28 09:01:27 +0530

    Fix Express 5 wildcard routing crash

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', apiRoutes);
 
 // Fallback for React SPA
-app.get('*', (req, res) => {
+app.get(/^.*$/, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
